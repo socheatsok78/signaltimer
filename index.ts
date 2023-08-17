7bd7e9e6088fc8385f303d4dd444a612553a6b15
Socheat Sok <socheatsok78@gmail.com>
2023-08-17 14:06:54 +0700

    Add requestSignalAnimationFrame

--- index.ts
+++ index.ts
@@ -31,3 +31,26 @@ export function setSignalTimeout(handler: Function, signal?: AbortSignal, ms?: n
     signal?.addEventListener('abort', clear)
     return clear
 }
+
+type CancelAnimationFrame = () => void
+
+/**
+ * A requestAnimationFrame() wrapper that with support for AbortSignal
+ * 
+ * **Note:** This function will call the handler immediately, and then call it again on the next animation frame.
+ * 
+ * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame)
+ */
+export function requestSignalAnimationFrame(handler: FrameRequestCallback, signal?: AbortSignal): CancelAnimationFrame {
+    let cancelled = false
+    function step(time: number) {
+        if (signal?.aborted) return
+        if (cancelled) return
+        handler(time)
+        requestAnimationFrame(step)
+    }
+    requestAnimationFrame(step)
+    const cancel = () => cancelled = true
+    signal?.addEventListener('abort', cancel)
+    return cancel
+}
